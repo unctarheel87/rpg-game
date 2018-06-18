0545453a1560245c49a0df07b4c972d49ca79202
John Tillman <john.w.tillman@gmail.com>
2018-06-18 19:10:27 -0400

    changes

--- game.js
+++ game.js
@@ -1,46 +1,79 @@
-const mario = {
+//character data
+const characters = [
+{
+  name: 'Mario',
   healthPoints: 180,
   attackPower: 10,
   counterAttackPower: 12
-};
-
-const donkeykong = {
+},
+{
+  name: 'Donkey Kong',
   healthPoints: 220,
   attackPower: 8,
   counterAttackPower: 20
-};
-
-const bowser = {
+},
+{
+  name: 'Bowser',
   healthPoints: 230,
   attackPower: 12,
   counterAttackPower: 14
-};
-
-const wario = {
+},
+{
+  name: 'Wario',
   healthPoints: 200,
   attackPower: 14,
   counterAttackPower: 10
-};
+}
+]
+
+let myChar = '';
+let myEnemy = '';
+let myCharData = null;
+let myEnemyData = null;
 
 $(document).ready(function() {
-  
-$('body').one('click', '.char', function() {
+
+  //choose char
+  $('body').one('click', '.char', function() {
     let xCord = $(this).position().left - 15;
-    $(this).animate({ top: '+=200px', left: '-=' + xCord }, 'normal')
+    $(this).animate({ top: '+=205px', left: '-=' + xCord }, 'normal');
     $('.char').not(this).each(function( i ) {
       let eachXCord = $(this).position().left - 15 - (i * 200);
-      $(this).animate({ top: '+=400px', left: '-=' + eachXCord }, 'normal').addClass('enemy');
+      $(this).animate({ top: '+=410px', left: '-=' + eachXCord }, 'normal').addClass('enemy');
     }).off('click'); 
+    myChar = $(this).attr('data-char');
+    
+    //find current char loop
+    for(let char of characters) {
+      if(myChar == char.name) {
+        myCharData = char
+      }
+    }
   });
 
+  //chose enemy to fight
   $('body').one('click', '.enemy', function() {
     let xCord = $(this).position().left - 15;
-    $(this).animate({ top: '+=200px', left: '-=' + xCord }, 'normal').addClass('defender-char');
+    $(this).animate({ top: '+=285px', left: '-=' + xCord }, 'normal').addClass('defender-char');
     $('.enemy').not(this).each(function( i ) {
       let eachXCord = $(this).position().left - 15 - (i * 200);
       $(this).animate({ left: '-=' + eachXCord }, 'normal').addClass('enemy').off('click');
-    }); 
+    });
+    myEnemy = $(this).attr('data-char'); 
+
+    //find current defender loop
+    for(let char of characters) {
+      if(myEnemy == char.name) {
+        myEnemyData = char
+      }
+    }
   });
+
+  //attack button
+    $('#attack').on('click', function() {
+        $('.your-attack').text('You hit ' + myEnemyData.name + ' for ' + myCharData.attackPower + ' damage.')
+        $('.enemy-attack').text(myEnemyData.name + ' hit you back for ' + myEnemyData.counterAttackPower + ' damage.')
+    });  
  
   // health points
   let marioHp = $('#mario-hp');
@@ -48,9 +81,9 @@ $('body').one('click', '.char', function() {
   let bowserHp = $('#bowser-hp');
   let warioHp = $('#wario-hp');
 
-  marioHp.text(mario.healthPoints + ' HP');
-  donkeykongHp.text(donkeykong.healthPoints + ' HP');
-  bowserHp.text(bowser.healthPoints + ' HP');
-  warioHp.text(wario.healthPoints + ' HP');
+  marioHp.text(characters[0].healthPoints + ' HP');
+  donkeykongHp.text(characters[1].healthPoints + ' HP');
+  bowserHp.text(characters[2].healthPoints + ' HP');
+  warioHp.text(characters[3].healthPoints + ' HP');
 
 });
