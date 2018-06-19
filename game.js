cc9fa4da446fe926eef3586a2516ffd4d5552754
John Tillman <john.w.tillman@gmail.com>
2018-06-18 21:26:27 -0400

    changes

--- game.js
+++ game.js
@@ -26,10 +26,24 @@ const characters = [
 }
 ]
 
-let myChar = '';
-let myEnemy = '';
+// health points
+let marioHp = $('.mario-hp');
+let donkeykongHp = $('.donkeykong-hp');
+let bowserHp = $('.bowser-hp');
+let warioHp = $('.wario-hp');
+
+marioHp.text(characters[0].healthPoints + ' HP');
+donkeykongHp.text(characters[1].healthPoints + ' HP');
+bowserHp.text(characters[2].healthPoints + ' HP');
+warioHp.text(characters[3].healthPoints + ' HP');
+
+//choice char data
+let myChar = ' ';
+let myEnemy = ' ';
 let myCharData = null;
 let myEnemyData = null;
+let myRemainingHP = ' ';
+let enemyRemainingHP = ' ';
 
 $(document).ready(function() {
 
@@ -49,6 +63,7 @@ $(document).ready(function() {
         myCharData = char
       }
     }
+    myRemainingHP = myCharData.healthPoints;
   });
 
   //chose enemy to fight
@@ -61,31 +76,33 @@ $(document).ready(function() {
     });
     myEnemy = $(this).attr('data-char'); 
 
-    //find current defender loop
+    //find current defender loop & sort data
     for(let char of characters) {
       if(myEnemy == char.name) {
         myEnemyData = char
       }
     }
+    enemyRemainingHP = myEnemyData.healthPoints;
   });
 
   //attack button
-    $('#attack').on('click', function() {
-      if(myCharData && myEnemyData) { 
-        $('.your-attack').text('You hit ' + myEnemyData.name + ' for ' + myCharData.attackPower + ' damage.')
-        $('.enemy-attack').text(myEnemyData.name + ' hit you back for ' + myEnemyData.counterAttackPower + ' damage.')
-      }
-    });  
- 
-  // health points
-  let marioHp = $('#mario-hp');
-  let donkeykongHp = $('#donkeykong-hp');
-  let bowserHp = $('#bowser-hp');
-  let warioHp = $('#wario-hp');
+  $('#attack').on('click', function() {
+    if(myCharData && myEnemyData) { 
+      $('.your-attack').text('You hit ' + myEnemyData.name + ' for ' + myCharData.attackPower + ' damage.')
+      $('.enemy-attack').text(myEnemyData.name + ' hit you back for ' + myEnemyData.counterAttackPower + ' damage.')
+      
+      //decrement HP
+      myRemainingHP = myRemainingHP - myCharData.counterAttackPower;
+      enemyRemainingHP = enemyRemainingHP - myEnemyData.counterAttackPower;
 
-  marioHp.text(characters[0].healthPoints + ' HP');
-  donkeykongHp.text(characters[1].healthPoints + ' HP');
-  bowserHp.text(characters[2].healthPoints + ' HP');
-  warioHp.text(characters[3].healthPoints + ' HP');
+      $('.char').each(function( i ) {
+        if($(this).attr('data-char') == myCharData.name) {
+          $('.hp', this).text(myRemainingHP + ' HP')
+        } else if($(this).attr('data-char') == myEnemyData.name) {
+          $('.hp', this).text(enemyRemainingHP + ' HP')
+        }
+      })
+    }  
+  });  
 
 });
